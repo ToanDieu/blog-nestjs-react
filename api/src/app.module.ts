67c1756e73e1255e1d79c06d5c41fa7f9c8c7d38
toandv <toan.dieu@navigosgroup.com>
2021-05-29 11:41:30 +0700

    add auth module, login, hash password

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -22,7 +23,8 @@ import { UserModule } from './user/user.module';
       password: process.env.DB_PASSWORD || 'password',
       database: process.env.DB_NAME
     }),
-    UserModule
+    UserModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
